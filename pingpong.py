cc1dad8b6b304b03edd585d4b42483702c78f8f8
raynahou <rayna88hou@gmail.com>
2025-02-06 19:00:38 +0700

    racket control

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+
 # parent class for sprites
 class GameSprite(sprite.Sprite):
     # class constructor
@@ -18,6 +19,25 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
+# work on the rackets/players
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    # make another one for left racket (update_l), keys: w (up) & s (down)
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
 # create game window
 win_width = 700
 win_height = 500
@@ -25,6 +45,11 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("PingPong")
 back = (200, 255, 255)
 window.fill(back)
+
+# creating ball and paddles
+racket1 = Player('racket.jpg', 30, 200, 4, 50, 150)
+racket2 = Player('racket.jpg', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis_ball.jpg', 200, 200, 4, 50, 50)
 
 # create game loop
 run = True
@@ -39,6 +64,11 @@ while run:
 
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
 
     display.update()
     clock.tick(FPS)
